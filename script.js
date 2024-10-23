26d39c42b8ddcd91ba8243571bd4f5ace2dae735
Sumit Verma <smt96700@gmail.com>
2024-10-23 14:36:16 +0530

    Initialise the project

---
+++ script.js
@@ -0,0 +1,14 @@
+const names= ['Ayushman', 'Tanishka', 'Sumit'];
+
+const displayNames= ()=>{
+    const namesList= document.getElementById('names-list');
+    namesList.innerHTML='';
+
+    names.forEach(name => {
+        const listItem= document.createElement('li');
+        listItem.textContent= name;
+        namesList.appendChild(listItem);
+    });
+};
+
+displayNames();
